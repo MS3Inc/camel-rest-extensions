d51233d327a7034f81f135ae8930de55d382b2a7
R. L. Ratcliffe <rlr@tcliffe.dev>
2021-02-25 11:26:44 -0500

    Resolve #4

---
+++ src/main/java/com/ms3_inc/tavros/extensions/rest/exception/ConflictException.java
@@ -0,0 +1,42 @@
+package com.ms3_inc.tavros.extensions.rest.exception;
+
+/*-
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import com.ms3_inc.tavros.extensions.rest.OperationResult;
+
+import java.util.Optional;
+
+/***
+ * This class is an exception thrown when there is a conflict with the current
+ * state of the resource, and the situation can be resolved.
+ */
+public class ConflictException extends RestException {
+	public ConflictException(OperationResult.Message message) {
+		super(message);
+	}
+
+	public ConflictException(Throwable cause, OperationResult.Message message) {
+		super(message, cause);
+	}
+
+	/***
+	 * @return 409 wrapped in an {@link Optional}
+	 */
+	@Override
+	public Optional<Integer> httpStatusCode() {
+		return Optional.of(409);
+	}
+}
